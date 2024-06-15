96c6b774bb5f8a1b3313f803680a6c5fc98ecde7
Bereket Aschalew <bereketaschalew1212@gmail.com>
2024-06-15 11:39:52 +0000

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,12 @@
+"use strict";
+var mni1=document.getElementById("mni");
+var subs=document.getElementsByClassName("subi");
+subs[0].onclick= function(){
+    mni.src=subs[0].src;
+}
+subs[1].onclick= function(){
+    mni.src=subs[1].src;
+}
+subs[2].onclick= function(){
+    mni.src=subs[2].src;
+}
